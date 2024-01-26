a5b299d356db6d6d19da6d54cb61afc0ab6175e1
mariovaquero <mvaquero143@gmail.com>
2024-01-25 19:20:44 -0500

    resume info added, more styling

--- vite-project/src/Components/Resume.jsx
+++ vite-project/src/Components/Resume.jsx
@@ -17,7 +17,7 @@ function Resume() {
           </div>
           
           
-          <div className='sm:text-left text-2xl'>
+          <div className='sm:text-left text-2xl font-bold'>
           TECHNICAL SKILLS
           </div>
           <div>
@@ -27,15 +27,19 @@ function Resume() {
             </p>
           </div>
 
-          <div className='sm:text-left text-2xl'>
+          <div className='sm:text-right text-2xl font-bold inline border-b-4 border-grey'>
             TECHNICAL PROJECTS
           </div>
-          <div>
-            <p className='font-bold sm:text-right text-2xl'>
+          <div></div>
+          <p className='font-bold sm:text-left text-2xl'>
               Game Dev Portfolio
+          </p>
+          <div className='items-center'>
+            <p>
+              Portfolio showing some games I developed early in my career 
             </p>
             <p>
-              <ul className=''>
+              <ul className='list-disc'>
                 <li>Made using HTML CSS and JavaScript</li>
                 <li>CSS to style</li>
                 <li>HTML makes up the basic structure</li>
@@ -44,6 +48,82 @@ function Resume() {
             </p>
           </div>
 
+          <p className='font-bold sm:text-left text-2xl inline border-b-4 border-grey'>
+            Simulations GUI
+          </p>
+          <div>
+            <p>
+              Program made using only Python that shows off different simulations
+            </p>
+            <p>
+              <ul className='list-disc'>
+                <li>Developed GUI to show different simulations</li>
+                <li>Current Simulations: Solar System, Sand/Gravity, and Conway's Game of Life</li>
+              </ul>
+            </p>
+          </div>
+
+          <p className='font-bold sm:text-left text-2xl inline border-b-4 border-grey'>
+            Video Game Viewer
+          </p>
+          <div>
+            <p>
+              Site made with my classmate Miguel, shows a list of games and some information on them
+            </p>
+            <p>
+              <ul className='list-disc'>
+                <li>Guided our project to list everything in out database properly</li>
+                <li>Programmed with React to show the information my team gathered</li>
+              </ul>
+            </p>
+          </div>
+
+          <p className='font-bold sm:text-left text-2xl inline border-b-4 border-grey'>
+            Neon Tails
+          </p>
+          <div>
+            <p>
+              Online art gallery for cat, dog, and cyberpunk-related images.
+            </p>
+            <p>
+              <ul className='light-disc'>
+                <li>Developed the backend of this Site using SQL, Flask, and bcrypt</li>
+                <li>Handled the dummy data in our database for the frontend to access</li>
+              </ul>
+            </p>
+          </div>
+
+          <p className='font-bold sm:text-left text-2xl inline border-b-4 border-grey'>
+            Goofy Racers
+          </p>
+          <div>
+            <p>
+              3D multiplayer racing game made entirely in Python
+            </p>
+            <p>
+              <ul className='light-disc'>
+                <li>Using the Ursina Game Engine for the gameplay</li>
+                <li>Using Ursina Networking to develop the multiplayer</li>                
+              </ul>
+            </p>
+          </div>
+          <div className='sm:text-right text-2xl font-bold'>
+            Experience
+          </div>
+          <div>
+            <p className='font-bold sm:text-left text-2xl inline border-b-4 border-gray-50'>
+              Urban Arts Partnership Manhattan New York
+            </p>
+            <p className=' inline border-b-4 border-grey'>
+              Assistant Teaching Artist           June 2020-Present
+            </p>
+            <p>
+              <ul className='light-disc'>
+                <li>Help Teach Game Design and Development in Unity 2D and 3D</li>
+                <li>Led Course on 5 weeks about Game Design Fundamentals</li>
+              </ul>
+            </p>
+          </div>
         </div>
 
       </div>
